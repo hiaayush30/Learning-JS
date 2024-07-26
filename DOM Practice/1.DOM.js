2392b843501aefec144bac1b6b9f87ca2bcccf37
hiaayush30 <hiaayush30@gmail.com>
2024-07-26 15:10:33 +0530

    initial commit

---
+++ DOM Practice/1.DOM.js
@@ -0,0 +1,7 @@
+//Document Object Model- the whole html tag
+// handle selection,change and events
+
+
+const btn=document.getElementsByTagName(button);
+const div=document.getElementsByTagName('div');
+

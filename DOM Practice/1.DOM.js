fb3b1d22988ef95880baf966eaefef519031d38c
hiaayush30 <hiaayush30@gmail.com>
2024-07-26 16:36:06 +0530

    learnt all DOM concepts

--- DOM Practice/1.DOM.js
+++ DOM Practice/1.DOM.js
@@ -1,7 +1,48 @@
-//Document Object Model- the whole html tag
+//Document Object Model- the whole html page as an object to be manipulated by JS
 // handle selection,change and events
 
 
-const btn=document.getElementsByTagName(button);
-const div=document.getElementsByTagName('div');
+const btn=document.querySelector('button');
+
+//handling events
+btn.addEventListener('mouseover',(eventInfo)=>{
+    //dblclick,mouseover,mouseleave...   
+    console.log(eventInfo);
+    //btn.textContent +=', Hello There!';
+    btn.innerHTML ='<b>Hello</b>';
+    btn.style.backgroundColor='red';
+})
+btn.addEventListener('mouseleave',(eventInfo)=>{
+    btn.innerHTML ='<b>come back!</b>';
+    btn.style.backgroundColor='red';
+})
+
+const div=document.querySelector('div');
+//selects the first div
+div.classList.add('yo');
+// div.classList.remove('yo');
+
+const divs=document.querySelectorAll('div');
+//all divs selected
+
+
+//creating and appending element
+const heading=document.createElement('h1');
+heading.innerText='hello there!';
+heading.id='myHeading';
+heading.classList.add('yo');
+document.querySelector('body').appendChild(heading);
+
+
+//removing element
+setTimeout(()=>{
+    // heading.remove();
+    //or
+    document.querySelector('body').removeChild(heading);
+},3000);
+
+//Event Object
+document.querySelector('body').addEventListener('mousemove',(event)=>{
+    console.log(event);
+})
 

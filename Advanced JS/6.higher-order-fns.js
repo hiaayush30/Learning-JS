82338ee82d59413889a663f44e3c57aec5362dba
hiaayush30 <hiaayush30@gmail.com>
2024-07-28 17:08:55 +0530

    Advanced Js and DOM

--- Advanced JS/6.higher-order-fns.js
+++
@@ -1,4 +0,0 @@
-//fn accepting fn as a parameter and/or returns a fn
-const arr=[1,2,3,4];
-
-arr.forEach((e)=>{console.log(e)});
--- Advanced JS/8.customEvents.js
+++
@@ -1,8 +0,0 @@
-const myEvent=new Event('chacha');
-
-document.querySelector('button').addEventListener('chacha',()=>{
-    alert('chacha event hua!');
-});
-
-//makes the event occur
-document.querySelector('button').dispatchEvent(myEvent);
